8ab2789c445665cd7a319cfe428400593c968cfb
Alejandro Chavez <41581013+ubiot-alejandro@users.noreply.github.com>
2022-03-07 12:44:52 -0400

    Update support.sh

--- support.sh
+++ support.sh
@@ -2,61 +2,55 @@
 
 # Variables
 id=0001
-end=1
 str=$(wg)
 
-# Init for loop ("end" can be changed to excecute it more times)
-for ((i=1;i<=end;i++));
-
-do
-    # Checking if reboot required
-    if [ $(curl -s https://raw.githubusercontent.com/ubiot-alejandro/support/main/tei.txt | grep 0001 | cut -d "=" -f3) == "R" ]; then
-        # Reading hours uptime
-        if [ $(uptime | cut -d " " -f5 | cut -d ":" -f1 | cut -d "," -f1) -gt 0 ]; then
+# Checking if reboot required
+if [ $(curl -s https://raw.githubusercontent.com/ubiot-alejandro/support/main/tei.txt | grep 0001 | cut -d "=" -f3) == "R" ]; then
+    # Reading hours uptime
+    if [ $(uptime | cut -d " " -f5 | cut -d ":" -f1 | cut -d "," -f1) -gt 0 ]; then
+        echo Rebooting
+        reboot
+    else 
+        # Reading minutes uptime
+        if [ $(uptime | cut -d " " -f5 | cut -d ":" -f2 | cut -d "," -f1) -gt 15 ]; then
             echo Rebooting
             reboot
-        else 
-            # Reading minutes uptime
-            if [ $(uptime | cut -d " " -f5 | cut -d ":" -f2 | cut -d "," -f1) -gt 15 ]; then
-                echo Rebooting
-                reboot
-            else
-                Not rebooting, waiting minutes...
-            fi
+        else
+            Not rebooting, waiting minutes...
         fi
     fi
+fi
 
-    # Reading the GitHub file to know if is needed to connect/disconnect to the VPN
-	if [ $(curl -s https://raw.githubusercontent.com/ubiot-alejandro/support/main/tei.txt | grep $id | cut -d "=" -f2) -eq 01 ]; then
-        str=$(wg)
-        # If the VPN is connected, don't connect again
-        if [ $( echo ${#str}) -ne 0 ]; then 
-            # Connection to the VPN
-            echo Connecting to the VPN...
-            echo 
-            uci set wireguard.@proxy[0].enable='1'
-            uci commit wireguard
-            /etc/init.d/wireguard restart
-            sleep 30
-            str=$(wg)
-        fi
-        echo VPN connected...
+# Reading the GitHub file to know if is needed to connect/disconnect to the VPN
+if [ $(curl -s https://raw.githubusercontent.com/ubiot-alejandro/support/main/tei.txt | grep $id | cut -d "=" -f2) -eq 01 ]; then
+    str=$(wg)
+    # If the VPN is connected, don't connect again
+    if [ $( echo ${#str}) -ne 0 ]; then 
+        # Connection to the VPN
+        echo Connecting to the VPN...
         echo 
-    else
-
+        uci set wireguard.@proxy[0].enable='1'
+        uci commit wireguard
+        /etc/init.d/wireguard restart
+        sleep 30
         str=$(wg)
-        # If the VPN is disconnected, don't disconnet again
-        if [ $(echo ${#str}) -ne 0 ]; then
-            # Disconnection to the VPN
-            echo Disconnecting to the VPN...
-            echo
-            uci set wireguard.@proxy[0].enable='0'
-            uci commit wireguard
-            /etc/init.d/wireguard restart
-            sleep 30
-            str=$(wg)
-        fi
-        echo VPN disconnected...
+    fi
+    echo VPN connected...
+    echo 
+else
+
+    str=$(wg)
+    # If the VPN is disconnected, don't disconnet again
+    if [ $(echo ${#str}) -ne 0 ]; then
+        # Disconnection to the VPN
+        echo Disconnecting to the VPN...
         echo
-    fi;
-done
+        uci set wireguard.@proxy[0].enable='0'
+        uci commit wireguard
+        /etc/init.d/wireguard restart
+        sleep 30
+        str=$(wg)
+    fi
+    echo VPN disconnected...
+    echo
+fi;
