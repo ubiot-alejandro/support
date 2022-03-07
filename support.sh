2d1606f64752e03d223e47a1239bbadb67d99dc1
Alejandro Chavez <41581013+ubiot-alejandro@users.noreply.github.com>
2022-03-07 12:41:10 -0400

    Update support.sh

--- support.sh
+++ support.sh
@@ -27,12 +27,10 @@ do
     fi
 
     # Reading the GitHub file to know if is needed to connect/disconnect to the VPN
-    if [ $(curl -s https://raw.githubusercontent.com/ubiot-alejandro/support/main/tei.txt | grep $id | cut -d "=" -f2) -eq 01 ]; then
-        
+	if [ $(curl -s https://raw.githubusercontent.com/ubiot-alejandro/support/main/tei.txt | grep $id | cut -d "=" -f2) -eq 01 ]; then
         str=$(wg)
         # If the VPN is connected, don't connect again
         if [ $( echo ${#str}) -ne 0 ]; then 
-        
             # Connection to the VPN
             echo Connecting to the VPN...
             echo 
@@ -42,16 +40,13 @@ do
             sleep 30
             str=$(wg)
         fi
-
         echo VPN connected...
         echo 
-
     else
 
         str=$(wg)
         # If the VPN is disconnected, don't disconnet again
         if [ $(echo ${#str}) -ne 0 ]; then
-
             # Disconnection to the VPN
             echo Disconnecting to the VPN...
             echo
@@ -60,12 +55,8 @@ do
             /etc/init.d/wireguard restart
             sleep 30
             str=$(wg)
-
         fi
-
         echo VPN disconnected...
         echo
-
     fi;
-
 done
